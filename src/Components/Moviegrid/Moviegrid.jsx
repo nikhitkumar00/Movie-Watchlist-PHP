eed7c0fb459b3fdd2d4d830da69814e9d0f1e44a
nikhitkumar <nikhitkumar00@gmail.com>
2023-05-21 11:12:06 +0530

    added buttons in movie boxes

--- src/Components/Moviegrid/Moviegrid.jsx
+++ src/Components/Moviegrid/Moviegrid.jsx
@@ -1,15 +1,26 @@
+import { Button } from "@mui/material";
 import "./Moviegrid.css";
 
 const Moviegrid = (props) => {
   return (
     <div className="movieWrapper">
       {props.movies.map((movie, index) => (
-        <div className="movieboxes" key={index}>
-          <div className="movieimage">
-            <img src={movie.Poster}></img>
+        <div>
+          <div className="movieboxes" key={index}>
+            <div className="movieimage">
+              <img src={movie.Poster}></img>
+            </div>
+            <h2 className="moviename">{movie.Title}</h2>
+            <h5 className="movieyear">{movie.Year}</h5>
+            <div className="movieboxaction">
+              <Button variant="contained" size="small">
+                Favourite
+              </Button>
+              <Button variant="outlined" size="small">
+                Watched
+              </Button>
+            </div>
           </div>
-          <h2 className="moviename">{movie.Title}</h2>
-          <h5 className="movieyear">{movie.Year}</h5>
         </div>
       ))}
     </div>
